288cc78accd62975a7899cebd456a5337dcb5804
Ted Summer <ted.summer2@gmail.com>
2019-04-22 11:00:05 -0500

    refactor(start_service): add conditional running

--- start_service.sh
+++ start_service.sh
@@ -1,34 +1,50 @@
 #! /bin/bash
 #
 # Helper for building and running paropt service
-# note that docker commands may require sudo permission
 #
 
-# If running workers on AWS (EC2Compute), you need to include the lines below, else remove them
-# this assumes you have the aws cli installed and configured on the machine
-# Note that other AWS env vars are required, see .example.env
-AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
-AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
-# parsl requires a file for keeping track of the vpcs and security groups it creates.
-provider_state_path=`pwd`/config/awsproviderstate.json
+function usage() {
+  echo "Usage: ./start_service [--aws | --local] (--build (<docker build flags))"
+}
 
-# optinally rebuild the image, passing arguments
+paropt_run_location=$1
+shift
+
+# optinally rebuild the image, passing remaining arguments
 if [[ $1 =~ build ]]; then
   shift
-  docker build . -t paropt-service:latest $@
+  sudo docker build . -t paropt-service:latest $@
 fi
 
-# the log path allows you to persist logs when running the optimizer
-logging_path=${HOME}/Documents/log/paropt
-# environment variables for the container
-env_config_path=config/.env.dev
+if [[ "${paropt_run_location}" =~ "prod" ]]; then
+  # If running workers on AWS (EC2Compute), you need to include the lines below
+  # this assumes you have the aws cli installed and configured on the machine
+  # Note that other AWS env vars are required, see .example.env
+  AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
+  AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
 
-# run the container
-# if not using AWS, remove the lines setting AWS environment variables
-sudo docker run -p 8080:8080 -p 54000-54100:54000-54100 \
-  -v ${provider_state_path}:/etc/awsproviderstate.json \
-  -v ${logging_path}:/var/log/paropt \
-  --env-file ${env_config_path} \
-  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-  paropt-service:latest
+  # environment variables for the container
+  source config/.env.prod
+
+  # run the container
+  sudo docker run -p 8080:8080 -p 54000-54100:54000-54100 \
+    -v ${PAROPT_HOST_PROVIDER_STATE}:/etc/awsproviderstate.json \
+    -v ${PAROPT_HOST_LOGS}:/var/log/paropt \
+    --env-file config/.env.prod \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    paropt-service:latest
+
+elif [[ "${paropt_run_location}" =~ "dev" ]]; then
+  # environment variables for the container
+  source config/.env.dev
+
+  # run the container
+  sudo docker run -p 8080:8080 -p 54000-54100:54000-54100 \
+    -v ${PAROPT_HOST_LOGS}:/var/log/paropt \
+    --env-file config/.env.dev \
+    paropt-service:latest
+else
+  usage
+  exit 1
+fi
