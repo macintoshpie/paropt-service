dd1254bb6a311557a578ba9df76adb2796e93cec
Ted Summer <ted.summer2@gmail.com>
2019-04-22 16:40:26 -0500

    fix(compose): add aws creds to prod config

--- start_compose.sh
+++ start_compose.sh
@@ -5,10 +5,13 @@
 
 if [[ $1 =~ prod$ ]]; then
   paropt_env="prod"
+  # assumes aws cli is installed
+  AWS_ACCESS_KEY_ID=$(aws --profile default configure get aws_access_key_id)
+  AWS_SECRET_ACCESS_KEY=$(aws --profile default configure get aws_secret_access_key)
 else
   paropt_env="dev"
 fi
 
 # catting env file b/c it contains some variables used in docker yaml file
 sudo env $(cat config/.env.${paropt_env} | sed '/^#/d') \
-  docker-compose -f docker-compose.yml -f docker-compose.${paropt_env}.yml up
+  docker-compose -f docker-compose.yml -f docker-compose.${paropt_env}.yml up $@
